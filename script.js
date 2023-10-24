4d14f90ac1e9ac44ec5bd54de3f119202042e442
Muhammad Hassan <hassanshakil12@gmail.com>
2023-10-25 04:51:57 +0500

    Automatic Image Slider

--- script.js
+++ script.js
@@ -6,16 +6,27 @@ const dropdownMenu = () => {
     shopBtn.addEventListener('mouseover', function () {
         dropdown.style.opacity = 1;
         content.style.filter = 'blur(5px)';
-        // dropdown.style.transform = 'translateY(0)';
     })
 
     dropdown.addEventListener('mouseleave', function () {
         dropdown.style.opacity = 0;
         content.style.filter = 'none'
-        // dropdown.style.transform = 'translateY(-30%)';
     })
-
-    
 }
 
 dropdownMenu();
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
