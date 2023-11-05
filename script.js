ef845817ff9c4b6ffcd522ac8336365903006ba7
Muhammad Hassan <hassanshakil12@gmail.com>
2023-11-05 05:19:27 +0500

    Navbar fixes

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let num = 0;
 const responsiveShopBtn = () => {
     let resShopBtn = document.querySelector('.sideMenu .sideBarShopBtn');
 
-    resShopBtn.addEventListener('click', function(){
-        if(num == 0){
+    resShopBtn.addEventListener('click', function () {
+        if (num == 0) {
             shopMenu.style.display = 'block';
             num = num + 1;
         }
-        else{
+        else {
             shopMenu.style.display = 'none';
             num = num - 1;
         }
@@ -24,19 +24,19 @@ const responsiveMenuSlider = () => {
     let menuText = document.querySelector('.sideMenu p');
     let count = 0
 
-    menu.addEventListener('click', function(){
-        if(count == 0){
+    menu.addEventListener('click', function () {
+        if (count == 0) {
             slider.style.width = '55vw';
             slider.style.opacity = 1;
             bg.style.filter = 'blur(7px) brightness(50%)';
             slider.style.display = 'block';
             count = count + 1;
         }
-        else{
+        else {
             if (num == 1) {
                 shopMenu.style.display = 'none';
                 num = num - 1;
-            }    
+            }
             slider.style.width = 0;
             slider.style.opacity = 0;
             bg.style.filter = 'blur(0) brightness(100%)';
@@ -54,7 +54,7 @@ const dropdownMenu = () => {
     shopBtn.addEventListener('mouseover', function () {
         dropdown.style.opacity = 1;
         content.style.filter = 'blur(5px)';
-        content.style.opacity = 0.3;
+        content.style.opacity = 0.5;
         dropdown.style.display = 'block';
     })
 
@@ -72,22 +72,28 @@ const searchFieldAnimation = () => {
     let cont = document.querySelector('.main .content');
     let click = 0;
 
-    btn.addEventListener('click', function(){
-        if(click==0){
-            searchInput.style.opacity = 1;
-            searchInput.style.transform = 'translateY(40vh)';
-            cont.style.filter = 'brightness(50%)';
+    btn.addEventListener('click', function () {
+        if (click == 0) {
+            searchInput.style.display = 'block';
             cont.style.filter = 'blur(10px)';
+            cont.style.opacity = 0.5;
             click = click + 1;
         }
-        else{
-            searchInput.style.opacity = 0;
-            searchInput.style.transform = 'translateY(-40vh)';
-            cont.style.filter = 'brightness(100%)';
+        else {
+            searchInput.style.display = 'none';
             cont.style.filter = 'blur(0)';
+            cont.style.opacity = 1;
             click = click - 1;
         }
-        
+    })
+
+    cont.addEventListener('click', function () {
+        if (click == 1) {
+            searchInput.style.display = 'none';
+            cont.style.filter = 'blur(0)';
+            cont.style.opacity = 1;
+            click = click - 1;
+        }
     })
 }
 
@@ -104,10 +110,10 @@ const showSlides = (n) => {
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
     if (n > slides.length) {
-         slideIndex = 1 
+        slideIndex = 1
     }
-    if (n < 1) { 
-        slideIndex = slides.length 
+    if (n < 1) {
+        slideIndex = slides.length
     }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
